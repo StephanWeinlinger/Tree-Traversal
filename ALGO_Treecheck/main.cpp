45fea25864fe9219c16d9796f8c723c50c50e87a
StephanWeinlinger <stephan.weinlinger@gmail.com>
2021-04-29 00:42:47 +0200

    add structure

---
+++ ALGO_Treecheck/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Controller.h"
+
+int main(int arc, char **argv) {
+	
+	switch(arc) {
+		case 2:
+			// 1st Task
+			break;
+		case 3:
+			// 2nd Task
+			break;
+		default:
+			std::cout << "Wrong parameter amount!" << std::endl;
+	}
+	return 0;
+}
